e530b01b828fc14ca2ddacbe7a7a11720f4f1fa4
Alessandro Willian Nogueira <alessandro.nogueira+caixa@caixa.gov.br>
2025-10-08 23:03:04 -0300

    ajuste overlay

--- src/components/DirectionalOverlay.tsx
+++ src/components/DirectionalOverlay.tsx
@@ -66,7 +66,7 @@ export default function DirectionalOverlay({
   return (
     <div
       style={{
-        position: "fixed",
+        position: "absolute",
         top: 0,
         left: 0,
         right: 0,
